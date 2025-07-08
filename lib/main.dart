bbd7b553d09050f5990c156cbbd512a6b609fa94
Volkov David <volkovdavid611@gmail.com>
2025-07-08 23:51:53 +0300

    auth fields & button

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,65 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(home: Scaffold(body: const AuthForm()));
+  }
+}
+
+class AuthForm extends StatelessWidget {
+  const AuthForm({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                hintText: 'Username',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                hintText: 'Password',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 50),
+            child: SizedBox(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  foregroundColor: WidgetStateProperty.all<Color>(Colors.black),
+                  side: WidgetStateProperty.all<BorderSide>(
+                    const BorderSide(color: Colors.black, width: 0.8),
+                  ),
+                ),
+
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Button pressed!')),
+                  );
+                },
+                child: const Text('Login'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
