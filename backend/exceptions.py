6961ff91f4f449070ee6692499a8b95a604e3e06
xllegan <ooushakov@edu.hse.ru>
2025-07-10 01:23:25 +0300

    added new excps for utils

--- backend/exceptions.py
+++ backend/exceptions.py
@@ -1,2 +1,11 @@
 class DataBaseURLNotFound(Exception):
     """DataBase URL cannot be parsed from .env file"""
+
+class SecretKeyNotFound(Exception):
+    """Secret Key cannot be parsed from .env file"""
+
+class InvalidSecretKey(Exception):
+    """Secret Key was parsed, but it is invalid"""
+
+class InvalidKeyType(Exception):
+    """You indicated wrong key type"""
