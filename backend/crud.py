b5c14abb9e4cb2b69ae82a25a2cf87f8445c78c3
xllegan <ooushakov@edu.hse.ru>
2025-07-10 05:45:17 +0300

    added auth CRUD

---
+++ backend/crud.py
@@ -0,0 +1,38 @@
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+
+from backend.database import AsyncSession
+from backend.models import User
+from backend.schemas import UserCreate
+from backend.utils import hash_password
+from backend.exceptions import UserAlreadyExists, UserNotFound
+
+
+async def get_user_by_login(session: AsyncSession, login: str) -> User | None:
+    result = await session.execute(select(User).where(User.login == login))
+    return result.scalar_one_or_none()
+
+async def create_user(session: AsyncSession, user_data: UserCreate) -> User:
+    hashed_password_ = hash_password(user_data.password)
+    user = User(login=user_data.login, hashed_password=hashed_password_)
+
+    session.add(user)
+    try:
+        await session.commit()
+        await session.refresh(user)
+        return user
+    except IntegrityError:
+        await session.rollback()
+        raise UserAlreadyExists("User with this login already exists")
+
+async def update_refresh_token(session: AsyncSession, user_id: int, refresh_token_: str | None):
+    result = await session.execute(select(User).where(User.id == user_id))
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise UserNotFound(f"User with id {user_id} not found")
+
+    user.refresh_token = refresh_token_
+    await session.commit()
+    await session.refresh(user)
+    return user
